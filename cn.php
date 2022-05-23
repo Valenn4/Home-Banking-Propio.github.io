b5daaf82edbb826d5be3ab8c7c4c36a0b0eb078f
Valenn4 <94750584+Valenn4@users.noreply.github.com>
2022-05-23 14:53:45 -0300

    Delete cn.php

--- cn.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-$conexion = mysqli_connect("localhost","root","","home banking");
-
-if(mysqli_connect_error()){
-    echo 'No se pudo conectar a la base de datos : '.mysqli_connect_error();
-}
-
-?>
